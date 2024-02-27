83903925cb8cbd2b9ba2b09c4fd7e05d720a2927
sorin-bogde <sbogde@gmail.com>
2024-02-27 18:11:57 +0000

    eslint-disable-next-line react-hooks/exhaustive-deps

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ function App() {
         ...historicalData[currentIndex],
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentIndex, historicalData]);
 
   const goToNextDay = () => {
